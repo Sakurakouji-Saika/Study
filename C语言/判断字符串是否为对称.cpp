6f29486c35367b40e49c5439654228443b07a4bb
萝莎莉娅·阿琳 <68391674+Rozaliya-Olenyeva@users.noreply.github.com>
2020-12-24 15:28:12 +0800

    更新 判断字符串是否为对称.cpp

--- C语言/判断字符串是否为对称.cpp
+++ C语言/判断字符串是否为对称.cpp
@@ -5,6 +5,7 @@ int main() {
 	char str1[30], str2[30];
 	printf("请输入一串字符串：");
 	gets(str1);//字符串输入
+	strcpy(str2, str1);//字符串2复制字符串1的的内容
 	strrev(str2);//将字符串倒置
 	if (strcmp(str1, str2) == 0) {
 		//strcmp 是判断2个字符串的值是否一样
