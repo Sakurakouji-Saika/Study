8486d21b60d7a272affaff7cd76a4f898b4f78a6
Saika <admin@moe.com>
2021-02-01 00:36:30 +0800

    1.0

--- 航电OJ/2009-求数列的和.cpp
+++ 航电OJ/2009-求数列的和.cpp
@@ -1,1 +1,21 @@
-#include<iostream> 
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int m, n;
+	while (cin >> n >> m)
+	{
+		double number = n;
+		double num = n;
+		for (int i = 1; i < m; i++)
+		{
+			num = sqrt(num);
+			number = number + num;
+		}
+		cout << fixed << setprecision(2) << number << endl;
+	} 
+	return 0;
+}
+
