739c3e208b8bef5af7642f72d14878561a6c6074
Saika <admin@moe.com>
2021-02-01 01:22:05 +0800

    1.0
    绝了。标识符不够用了

--- 航电OJ/2010-水仙花数.cpp
+++ 航电OJ/2010-水仙花数.cpp
@@ -1,5 +1,38 @@
-#include<iosteam>
+#include <iostream>
 using namespace std;
-int main(){
-	
+int main()
+{
+	int m, n;
+	while ( cin >> m >> n )
+	{
+		int	count = 0;
+		int	a[10];
+		for ( int i = m; i <= n; i++ )
+		{
+			int bai, ge, shi;
+			bai	= i / 100;
+			shi	= (i - bai * 100) / 10;
+			ge	= i - bai * 100 - shi * 10;
+			if ( i == bai * bai * bai + ge * ge * ge + shi * shi * shi )
+			{
+				a[count] = i;
+				count++;
+			}
+		}
+		if ( count == 0 )
+		{
+			cout << "no" << endl;
+		}else{
+			for ( int j = 0; j < count; j++ )
+			{
+				cout << a[j];
+				if ( j != count - 1 )
+				{
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+	}
+	return(0);
 }
